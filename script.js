935fdf49bed4eb968dd8621019c2f09c9a2f2818
Ranjeet Kumar Yadav <138971007+Ranjeet5644@users.noreply.github.com>
2024-05-07 18:22:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Hello world");
+console.log("Code is running...");
+console.log("Code is also  running...");
+console.log("Code is looking  wow...");
+
+var a = prompt("Enter your number: ");
+console.log("Your numbeer is " +a);
+
+var isTrue = confirm("are you sure leave this page and blast computer");
+if(isTrue){
+   console.log("Blast the computer");
+
+
+} else{
+
+    console.log(" Not Blast the computer");
+
+}
+console.log("Your numbeer is " +a);
